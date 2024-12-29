0179e6e2f63d81eef92292424ef08fb0dee3c377
anishsinghQB <anish.singh@quadbtech.com>
2024-12-29 19:04:42 +0530

    fix front-end : resolve error in TaskListbtn

--- frontend/src/components/taskList/TaskListBtn/TaskListbtn.tsx
+++ frontend/src/components/taskList/TaskListBtn/TaskListbtn.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const TaskListbtn:React.FC<{color:string,icon?:any,title:string,onClick:()=>void}> = ({ color = "blue", icon,title,onClick }) => {
+const TaskListbtn:React.FC<{color:string,icon?:any,title:string,onClick:()=>void}> = ({ color = "blue", icon,title }) => {
   return (
-    <button onClick={onClick} className={`p-2 border-2 rounded-sm border-${color}-700 text-${color}-700 hover:bg-${color}-200`}>
+    <button  className={`p-2 border-2 rounded-sm border-${color}-700 text-${color}-700 hover:bg-${color}-200`}>
       <span className="w-[10px]">{icon}</span> {title}
     </button>
   );
